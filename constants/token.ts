195e08e2efa37ceef3ec94c86fef996306685fe0
flyinglory <93647809+flyinglory@users.noreply.github.com>
2021-11-12 08:18:20 +1100

    Validate erc20 address (#12)
    * Add validateErc20Address function
    
    * Use validateErc20Address in onCreateOption
    
    Co-authored-by: Joachim Bachstätter <joachim.bachstatter@gmail.com>

--- constants/token.ts
+++ constants/token.ts
@@ -1,3 +1,6 @@
+import { erc20Abi } from 'contracts/erc20';
+import { ethers } from 'ethers';
+
 export const TokenListUrls = {
 	Synthetix: 'https://synths.snx.eth.link',
 	OneInch: 'https://gateway.ipfs.io/ipns/tokens.1inch.eth',
@@ -32,4 +35,45 @@ export const ether = {
 	name: 'Ethereum',
 	symbol: 'ETH',
 	tags: [],
+};
+type ValidateErc20AddressReturn =
+	| { result: 'success'; token: Token }
+	| { result: 'failure'; errorMessage: string };
+
+export const validateErc20Address = async (
+	address: string,
+	provider: ethers.providers.Provider | null
+): Promise<ValidateErc20AddressReturn> => {
+	if (!provider) return { result: 'failure', errorMessage: 'Wallet not connected' };
+	const contract = new ethers.Contract(address, erc20Abi, provider);
+	const [name, symbol, decimals, totalSupply] = await Promise.all([
+		contract.name(),
+		contract.symbol(),
+		contract.decimals(),
+		contract.totalSupply(),
+	]).catch(() => []);
+
+	if (
+		typeof name === 'string' &&
+		typeof symbol === 'string' &&
+		typeof decimals === 'number' &&
+		totalSupply !== undefined
+	) {
+		const token: Token = {
+			address,
+			symbol,
+			name,
+			decimals,
+			chainId: 1,
+			tags: [],
+			logoURI: '',
+		};
+
+		return {
+			result: 'success',
+			token,
+		};
+	}
+
+	return { result: 'failure', errorMessage: 'Not a valid ERC20 address' };
 };
