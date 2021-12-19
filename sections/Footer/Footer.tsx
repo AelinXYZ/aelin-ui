bef49d0d74aa9b99a9e1aa1f8f4eec8948ccee14
slorenzo <sebastianlorenzo@gmail.com>
2021-12-19 17:47:03 -0300

    add homepage design

--- sections/Footer/Footer.tsx
+++ sections/Footer/Footer.tsx
@@ -1,6 +1,8 @@
 import { FC } from 'react';
 import styled from 'styled-components';
 import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 import TwitterLogo from 'assets/svg/twitter-logo.svg';
 import DiscordLogo from 'assets/svg/discord-logo.svg';
@@ -17,17 +19,51 @@ const LOGOS = [
 	{ name: 'github', logo: GithubLogo, url: 'https://github.com/AelinXYZ' },
 ];
 
-const Footer: FC = () => (
-	<Container>
-		<SocialRow>
-			{LOGOS.map(({ name, logo, url }) => (
-				<Link key={`${name}-link`} href={url} target="_blank">
-					<Image src={logo} alt={`${name} logo`} />
-				</Link>
-			))}
-		</SocialRow>
-	</Container>
-);
+const Footer: FC = () => {
+	const { asPath } = useRouter();
+	console.log('asPath: ', asPath);
+
+	return (
+		<Container>
+			{asPath === '/home' ? (
+				// TODO: Define which one will stay here
+				<NavColumn>
+					<div>
+						<Link href="/home" passHref>
+							<StyledA>HOME. </StyledA>
+						</Link>
+						<Link href="/?" passHref>
+							<StyledA>KNOWLEDGEBASE. </StyledA>
+						</Link>
+						<Link href="/about" passHref>
+							<StyledA>ABOUT. </StyledA>
+						</Link>
+						<Link href="/pools" passHref>
+							<StyledA>JOIN POOL. </StyledA>
+						</Link>
+						<Link href="/pools/create" passHref>
+							<StyledA>CREATE POOL. </StyledA>
+						</Link>
+					</div>
+					<div>
+						<StyledP>
+							©COPYRIGHT AELIN 2021. ALL RIGHTS RESERVED.{' '}
+							<StyledNews>SIGN UP FOR AELIN NEWS</StyledNews>
+						</StyledP>
+					</div>
+				</NavColumn>
+			) : (
+				<SocialRow>
+					{LOGOS.map(({ name, logo, url }) => (
+						<a key={`${name}-link`} href={url} target="_blank" rel="noreferrer">
+							<Image src={logo} alt={`${name} logo`} />
+						</a>
+					))}
+				</SocialRow>
+			)}
+		</Container>
+	);
+};
 
 const Container = styled.div`
 	margin-top: 20px;
@@ -43,6 +79,27 @@ const SocialRow = styled.div`
 	justify-content: space-between;
 `;
 
-const Link = styled.a``;
+const NavColumn = styled.div`
+	display: flex;
+	flex-direction: column;
+`;
+
+const StyledA = styled.a`
+	color: #ffffff;
+	font-size: 12px;
+	letter-spacing: 0.62px;
+	margin: 0 3px;
+`;
+
+const StyledP = styled.p`
+	color: #ffffff;
+	font-size: 12px;
+	letter-spacing: 0.62px;
+`;
+
+const StyledNews = styled.a`
+	color: #36a3a3;
+	cursor: pointer;
+`;
 
 export default Footer;
