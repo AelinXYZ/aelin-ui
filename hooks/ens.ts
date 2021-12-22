0a58de524441d2594bc376c9da219a9d44d89448
0xcdb <90234851+0xcdb@users.noreply.github.com>
2021-12-22 16:01:10 +1100

    UI tweaks (#83)
    * Block explorer URL fixed for OP + Gas Prices fix
    
    * console.log removed
    
    * query dev tool added
    
    * subgraph fix
    
    * switch network fixed
    
    * more UI fixes
    
    * renaming
    
    * another fix
    
    * Alpha tag added
    
    * ENS fix
    
    Co-authored-by: 0xcb <>

--- hooks/ens.ts
+++ hooks/ens.ts
@@ -1,15 +1,22 @@
-import { isAddress } from '@ethersproject/address';
+import { useMemo } from 'react';
+import { utils, providers } from 'ethers';
 import Connector from 'containers/Connector';
 import { useEffect, useState } from 'react';
 import { NetworkId } from 'constants/networks';
 
 export const isEns = (x: string) => x.endsWith('.eth');
 export const useEnsToAddress = (ensName: string) => {
-	const { provider, network } = Connector.useContainer();
+	const { network } = Connector.useContainer();
 	const [address, setAddress] = useState(ensName);
+	const provider = useMemo(
+		() =>
+			new providers.InfuraProvider(NetworkId.Mainnet, process.env.NEXT_PUBLIC_INFURA_PROJECT_ID),
+		[]
+	);
+
 	useEffect(() => {
 		const getAddress = async () => {
-			if (!isEns(ensName) || network.id !== NetworkId.Mainnet) return ensName;
+			if (!isEns(ensName)) return ensName;
 			const address = await provider?.resolveName(ensName);
 			setAddress(address || ensName);
 		};
@@ -19,11 +26,16 @@ export const useEnsToAddress = (ensName: string) => {
 };
 
 export const useAddressToEns = (address: string) => {
-	const { provider, network } = Connector.useContainer();
+	const { network } = Connector.useContainer();
+	const provider = useMemo(
+		() =>
+			new providers.InfuraProvider(NetworkId.Mainnet, process.env.NEXT_PUBLIC_INFURA_PROJECT_ID),
+		[]
+	);
 	const [ensName, setEnsName] = useState(address);
 	useEffect(() => {
 		const getEnsName = async () => {
-			if (!isAddress(address) || network.id !== NetworkId.Mainnet) return address;
+			if (!utils.isAddress(address)) return address;
 			const resolvedEnsName = await provider?.lookupAddress(address);
 			// ENS does not enforce the accuracy of reverse records - for instance, anyone may claim that the name for their address is 'alice.eth'. To be certain that the claim is accurate, you must always perform a forward resolution for the returned name and check it matches the original address.
 			const resolvedAddress = resolvedEnsName
