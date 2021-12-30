645bac85682d0f2bd014bc22b209497bda0c6e4f
0xcb <>
2021-12-29 19:10:48 -0500

    remove claim from stakeActionLabel

--- sections/Stake/constants.ts
+++ sections/Stake/constants.ts
@@ -1,7 +1,6 @@
 export enum StakeActionLabel {
 	DEPOSIT = 'deposit',
 	WITHDRAW = 'withdraw',
-	CLAIM = 'claim',
 }
 
 export const StakeActions = [StakeActionLabel.DEPOSIT, StakeActionLabel.WITHDRAW];
