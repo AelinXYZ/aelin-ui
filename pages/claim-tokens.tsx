05761dfebcd4aaef3cdd2e4a50ae5cdd2220c465
Aelin-xyz <89638770+Aelin-xyz@users.noreply.github.com>
2022-01-06 11:39:11 +1100

    add merkle data to the client - still need to deploy the contract and… (#144)
    * add merkle data to the client - still need to deploy the contract and change the address and update the hashes file
    
    * update distribution hashes for aelin pool and reformat file for vAELIN merkle tree and update pool factory addresses
    
    Co-authored-by: alextheboredape <alextheboredape@protonmail.com>

--- pages/claim-tokens.tsx
+++ pages/claim-tokens.tsx
@@ -7,8 +7,10 @@ import Link from 'next/link';
 import { DEFAULT_NETWORK_ID } from 'constants/defaults';
 import useGetAirdropDataForAddress from 'queries/airdrop/useGetAirdropDataForAddress';
 import useGetCanClaimForAddress from 'queries/airdrop/useGetCanClaimForAddress';
+import useGetMerkleDataForAddress from 'queries/merkle/useGetMerkleDataForAddress';
+import useGetAddressCanClaimMerkle from 'queries/merkle/useGetAddressCanClaimMerkle';
 import { PageLayout } from 'sections/Layout';
-import { FlexDivColCentered } from 'components/common';
+import { FlexDivColCentered, FlexDivRow } from 'components/common';
 import Button from 'components/Button';
 import { wei } from '@synthetixio/wei';
 import ConfirmTransactionModal from 'components/ConfirmTransactionModal';
@@ -18,24 +20,53 @@ import Connector from 'containers/Connector';
 import { getGasEstimateWithBuffer } from 'utils/network';
 import TransactionNotifier from 'containers/TransactionNotifier';
 import DistributionContract from 'containers/ContractsInterface/contracts/AelinDistribution';
+import SecondDistributionContract from 'containers/ContractsInterface/contracts/SecondAelinDistribution';
 import { getKeyValue } from 'utils/helpers';
 import ROUTES from 'constants/routes';
 
 const Airdrop = () => {
-	const [showTxModal, setShowTxModal] = useState<boolean>(false);
-	const [gasLimitEstimate, setGasLimitEstimate] = useState<GasLimitEstimate>(null);
+	const [showAelinTxModal, setShowAelinTxModal] = useState<boolean>(false);
+	const [showvAelinTxModal, setShowvAelinTxModal] = useState<boolean>(false);
+	const [gasAelinLimitEstimate, setAelinGasLimitEstimate] = useState<GasLimitEstimate>(null);
+	const [gasvAelinLimitEstimate, setvAelinGasLimitEstimate] = useState<GasLimitEstimate>(null);
 	const { gasPrice, setGasPrice } = TransactionData.useContainer();
 	const { monitorTransaction } = TransactionNotifier.useContainer();
 	const { walletAddress, network, signer } = Connector.useContainer();
+
+	// first drop of AELIN
+	const merkleDataQuery = useGetMerkleDataForAddress();
+	const merkleBalance = merkleDataQuery?.data?.balance ?? null;
+	const merkleIndex = merkleDataQuery?.data?.index ?? null;
+	const merkleProof = merkleDataQuery?.data?.proof ?? null;
+	// first drop of vAELIN
 	const airdropDataQuery = useGetAirdropDataForAddress();
 	const airdropBalance = airdropDataQuery?.data?.balance ?? null;
 	const airdropIndex = airdropDataQuery?.data?.index ?? null;
 	const airdropProof = airdropDataQuery?.data?.proof ?? null;
 
+	// first drop of AELIN
+	const canClaimMerkleQuery = useGetAddressCanClaimMerkle(merkleIndex);
+	const canClaimMerkle = canClaimMerkleQuery?.data ?? false;
+	// first drop of vAELIN
 	const canClaimAirdropQuery = useGetCanClaimForAddress(airdropIndex);
 	const canClaim = canClaimAirdropQuery?.data ?? false;
 
+	// first drop of AELIN
 	const aelinDistributionContract = useMemo(() => {
+		if (!signer || !network?.id) return null;
+		const secondDistributionContract = (getKeyValue(SecondDistributionContract) as any)(
+			network?.id ?? DEFAULT_NETWORK_ID
+		);
+		if (!secondDistributionContract) return null;
+		return new ethers.Contract(
+			secondDistributionContract.address,
+			secondDistributionContract.abi,
+			signer
+		);
+	}, [signer, network?.id]);
+
+	// first drop of vAELIN
+	const vAelinDistributionContract = useMemo(() => {
 		if (!signer || !network?.id) return null;
 		const distributionContract = (getKeyValue(DistributionContract) as any)(
 			network?.id ?? DEFAULT_NETWORK_ID
@@ -44,20 +75,49 @@ const Airdrop = () => {
 		return new ethers.Contract(distributionContract.address, distributionContract.abi, signer);
 	}, [signer, network?.id]);
 
+	// first drop of AELIN
 	useEffect(() => {
 		const getGasLimitEstimate = async () => {
-			if (!walletAddress || !airdropBalance || !canClaim || !aelinDistributionContract) return;
+			if (!walletAddress || !merkleBalance || !canClaimMerkle || !aelinDistributionContract) return;
 			try {
 				const gasEstimate = await aelinDistributionContract.estimateGas.claim(
+					merkleIndex,
+					walletAddress,
+					merkleBalance,
+					merkleProof
+				);
+				setAelinGasLimitEstimate(wei(gasEstimate, 0));
+			} catch (e) {
+				console.log(e);
+				setAelinGasLimitEstimate(null);
+			}
+		};
+		getGasLimitEstimate();
+	}, [
+		merkleBalance,
+		walletAddress,
+		network?.id,
+		merkleIndex,
+		merkleProof,
+		canClaimMerkle,
+		aelinDistributionContract,
+	]);
+
+	// first drop of vAELIN
+	useEffect(() => {
+		const getGasLimitEstimate = async () => {
+			if (!walletAddress || !airdropBalance || !canClaim || !vAelinDistributionContract) return;
+			try {
+				const gasEstimate = await vAelinDistributionContract.estimateGas.claim(
 					airdropIndex,
 					walletAddress,
 					airdropBalance,
 					airdropProof
 				);
-				setGasLimitEstimate(wei(gasEstimate, 0));
+				setvAelinGasLimitEstimate(wei(gasEstimate, 0));
 			} catch (e) {
 				console.log(e);
-				setGasLimitEstimate(null);
+				setvAelinGasLimitEstimate(null);
 			}
 		};
 		getGasLimitEstimate();
@@ -68,24 +128,69 @@ const Airdrop = () => {
 		airdropIndex,
 		airdropProof,
 		canClaim,
-		aelinDistributionContract,
-		showTxModal,
+		vAelinDistributionContract,
 	]);
 
+	const isMerkleSubmitButtonDisabled = !merkleBalance || !canClaimMerkle;
 	const isSubmitButtonDisabled = !airdropBalance || !canClaim;
 
-	const handleClaim = useCallback(async () => {
-		if (isSubmitButtonDisabled || !gasLimitEstimate || !aelinDistributionContract) return;
+	// first drop of AELIN
+	const handleAelinClaim = useCallback(async () => {
+		if (isMerkleSubmitButtonDisabled || !gasAelinLimitEstimate || !aelinDistributionContract)
+			return;
 		try {
-			setShowTxModal(false);
+			setShowAelinTxModal(false);
 
 			const tx = await aelinDistributionContract.claim(
+				merkleIndex,
+				walletAddress,
+				merkleBalance,
+				merkleProof,
+				{
+					gasLimit: getGasEstimateWithBuffer(gasAelinLimitEstimate)?.toString(),
+					gasPrice: gasPrice.toBN(),
+				}
+			);
+			if (tx) {
+				monitorTransaction({
+					txHash: tx.hash,
+					onTxConfirmed: () => {
+						setTimeout(() => {
+							canClaimMerkleQuery.refetch();
+						}, 5 * 1000);
+					},
+				});
+			}
+		} catch (e) {
+			console.log(e);
+			setAelinGasLimitEstimate(null);
+		}
+	}, [
+		aelinDistributionContract,
+		merkleBalance,
+		merkleIndex,
+		merkleProof,
+		canClaimMerkleQuery,
+		gasAelinLimitEstimate,
+		gasPrice,
+		monitorTransaction,
+		isMerkleSubmitButtonDisabled,
+		walletAddress,
+	]);
+
+	// first drop of vAELIN
+	const handlevAelinClaim = useCallback(async () => {
+		if (isSubmitButtonDisabled || !gasvAelinLimitEstimate || !vAelinDistributionContract) return;
+		try {
+			setShowvAelinTxModal(false);
+
+			const tx = await vAelinDistributionContract.claim(
 				airdropIndex,
 				walletAddress,
 				airdropBalance,
 				airdropProof,
 				{
-					gasLimit: getGasEstimateWithBuffer(gasLimitEstimate)?.toString(),
+					gasLimit: getGasEstimateWithBuffer(gasvAelinLimitEstimate)?.toString(),
 					gasPrice: gasPrice.toBN(),
 				}
 			);
@@ -101,15 +206,15 @@ const Airdrop = () => {
 			}
 		} catch (e) {
 			console.log(e);
-			setGasLimitEstimate(null);
+			setvAelinGasLimitEstimate(null);
 		}
 	}, [
-		aelinDistributionContract,
+		vAelinDistributionContract,
 		airdropBalance,
 		airdropIndex,
 		airdropProof,
 		canClaimAirdropQuery,
-		gasLimitEstimate,
+		gasvAelinLimitEstimate,
 		gasPrice,
 		monitorTransaction,
 		isSubmitButtonDisabled,
@@ -122,34 +227,68 @@ const Airdrop = () => {
 				<title>Aelin - Claim Tokens</title>
 			</Head>
 
-			<PageLayout title={<>vAelin Distribution</>} subtitle="">
-				<Row>
-					<P>
-						{network.id !== NetworkId['Mainnet-ovm']
-							? 'Please switch to the Optimism Network by clicking the network tab in the top right'
-							: `Stakers on both L1 and L2 are eligible for vAELIN distribution. Click claim to get your vAELIN. Once claimed you can convert vAELIN into AELIN via the vAELIN pool on the pools page.`}
-					</P>
-					<Header>{`Allocation: ${ethers.utils.formatEther(airdropBalance ?? 0)} vAELIN`}</Header>
-					<SubmitButton
-						disabled={isSubmitButtonDisabled}
-						onClick={() => setShowTxModal(true)}
-						variant="text"
-					>
-						{canClaim ? 'Claim' : !airdropBalance ? 'Nothing to Claim' : 'Already Claimed'}
-					</SubmitButton>
-				</Row>
-				<Row>
-					<Link href={ROUTES.Pools.PoolView('0x3074306c0cc9200602bfc64beea955928dac56dd')} passHref>
-						<Anchor>Go to the vAELIN Pool</Anchor>
-					</Link>
-				</Row>
+			<PageLayout title={<>$AELIN + $vAELIN Distribution</>} subtitle="">
+				<FlexDivRow>
+					<Row>
+						<Header>{`Allocation: ${ethers.utils.formatEther(merkleBalance ?? 0)} AELIN`}</Header>
+						<SubmitButton
+							disabled={isMerkleSubmitButtonDisabled}
+							onClick={() => setShowAelinTxModal(true)}
+							variant="text"
+						>
+							{canClaimMerkle ? 'Claim' : !merkleBalance ? 'Nothing to Claim' : 'Already Claimed'}
+						</SubmitButton>
+						<P>
+							{network.id !== NetworkId['Mainnet-ovm']
+								? 'Please switch to the Optimism Network by clicking the network tab in the top right'
+								: `If you participated in the pro rata phase of the initial AELIN Pool you may claim your $AELIN tokens here. Retro payments to volunteer devs are also claimable here. Finally, eligible SNX stakers who submitted their addresses before Dec 28th can claim their tokens here. Those who are eligible but submitted after Dec 28 will receive their AELIN tokens after the Jan 15th submittal deadline`}
+						</P>
+					</Row>
+					<div>
+						<Row>
+							<Header>{`Allocation: ${ethers.utils.formatEther(
+								airdropBalance ?? 0
+							)} vAELIN`}</Header>
+							<SubmitButton
+								disabled={isSubmitButtonDisabled}
+								onClick={() => setShowvAelinTxModal(true)}
+								variant="text"
+							>
+								{canClaim ? 'Claim' : !airdropBalance ? 'Nothing to Claim' : 'Already Claimed'}
+							</SubmitButton>
+							<P>
+								{network.id !== NetworkId['Mainnet-ovm']
+									? 'Please switch to the Optimism Network by clicking the network tab in the top right'
+									: `Stakers on both L1 and L2 are eligible for vAELIN distribution. Click claim to get your vAELIN. Once claimed you can convert vAELIN into AELIN via the vAELIN pool on the pools page.`}
+							</P>
+						</Row>
+						<Row>
+							<Link
+								href={ROUTES.Pools.PoolView('0x3074306c0cc9200602bfc64beea955928dac56dd')}
+								passHref
+							>
+								<Anchor>Go to the vAELIN Pool</Anchor>
+							</Link>
+						</Row>
+					</div>
+				</FlexDivRow>
 				<ConfirmTransactionModal
 					title="Confirm Transaction"
-					setIsModalOpen={setShowTxModal}
-					isModalOpen={showTxModal}
+					setIsModalOpen={setShowAelinTxModal}
+					isModalOpen={showAelinTxModal}
 					setGasPrice={setGasPrice}
-					gasLimitEstimate={gasLimitEstimate}
-					onSubmit={handleClaim}
+					gasLimitEstimate={gasAelinLimitEstimate}
+					onSubmit={handleAelinClaim}
+				>
+					{`You are claiming ${ethers.utils.formatEther(merkleBalance ?? 0)} AELIN`}
+				</ConfirmTransactionModal>
+				<ConfirmTransactionModal
+					title="Confirm Transaction"
+					setIsModalOpen={setShowvAelinTxModal}
+					isModalOpen={showvAelinTxModal}
+					setGasPrice={setGasPrice}
+					gasLimitEstimate={gasvAelinLimitEstimate}
+					onSubmit={handlevAelinClaim}
 				>
 					{`You are claiming ${ethers.utils.formatEther(airdropBalance ?? 0)} vAELIN`}
 				</ConfirmTransactionModal>
---
+++ queries/merkle/useGetMerkleDataForAddress.ts
@@ -0,0 +1,47 @@
+import Connector from 'containers/Connector';
+import { useQuery } from 'react-query';
+import { isMainnet, NetworkId } from 'constants/networks';
+
+type MerkleRecord = {
+	balance: string;
+	index: number;
+	proof: string[];
+};
+
+const useGetMerkleDataForAddress = () => {
+	const { walletAddress, isOVM, network } = Connector.useContainer();
+	const isOnMainnet = isMainnet(network?.id ?? NetworkId.Mainnet);
+
+	return useQuery<MerkleRecord | null>(
+		['merkle', 'data', walletAddress, network?.id],
+		async () => {
+			if (walletAddress == null) {
+				return null;
+			}
+			const request = await fetch('/data/second-dist-hashes.json');
+			const merkleSource = await request.json();
+			const claimAccounts = Object.keys(merkleSource.claims).map((e) => e.toLowerCase());
+			const claimAccountsArr = Object.keys(merkleSource.claims).map((ele) => {
+				return {
+					address: ele.toLowerCase(),
+					index: merkleSource.claims[ele]['index'],
+					amount: merkleSource.claims[ele]['amount'],
+					proof: merkleSource.claims[ele]['proof'],
+				};
+			});
+			if (claimAccounts.includes(walletAddress)) {
+				return {
+					proof: claimAccountsArr[claimAccounts.indexOf(walletAddress)].proof, //get the proof
+					index: claimAccountsArr[claimAccounts.indexOf(walletAddress)].index, //get the index
+					balance: claimAccountsArr[claimAccounts.indexOf(walletAddress)].amount, //get the airdrop amount
+				};
+			}
+			return null;
+		},
+		{
+			enabled: !!walletAddress && isOVM && isOnMainnet,
+		}
+	);
+};
+
+export default useGetMerkleDataForAddress;
