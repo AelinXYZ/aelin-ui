eba57ac67d721683e546cfb7425f9c3372359438
0xcb <>
2022-02-09 14:07:18 -0500

    staking rewards refactoring

--- containers/ContractsInterface/ContractsInterface.tsx
+++ containers/ContractsInterface/ContractsInterface.tsx
@@ -3,20 +3,13 @@ import { Contract } from 'ethers';
 import { createContainer } from 'unstated-next';
 
 import PoolFactoryContract from './contracts/AelinPoolFactory';
-import AelinStakingRewardsContract from './contracts/AelinStakingRewards';
-import AelinEthStakingRewardsContract from './contracts/AelinEthStakingRewards';
-import AelinTokenContract from './contracts/AelinToken';
-import AelinEthLPTokenContract from './contracts/AelinEthLPToken';
 
 import Connector from 'containers/Connector';
 import { DEFAULT_NETWORK_ID } from 'constants/defaults';
 import { getKeyValue } from 'utils/helpers';
-import { StakingContracts } from './constants';
 
 type AelinContracts = {
 	AelinPoolFactory: Contract | null;
-	AelinStaking: StakingContracts | null;
-	AelinEthStaking: StakingContracts | null;
 };
 
 const useContractsInterface = () => {
@@ -30,18 +23,6 @@ const useContractsInterface = () => {
 			const poolFactoryContract = (getKeyValue(PoolFactoryContract) as any)(
 				network?.id ?? DEFAULT_NETWORK_ID
 			);
-			const aelinStakingRewardsContract = (getKeyValue(AelinStakingRewardsContract) as any)(
-				network?.id ?? DEFAULT_NETWORK_ID
-			);
-			const aelinEthStakingRewardsContract = (getKeyValue(AelinEthStakingRewardsContract) as any)(
-				network?.id ?? DEFAULT_NETWORK_ID
-			);
-			const aelinTokenContract = (getKeyValue(AelinTokenContract) as any)(
-				network?.id ?? DEFAULT_NETWORK_ID
-			);
-			const aelinEthLPTokenContract = (getKeyValue(AelinEthLPTokenContract) as any)(
-				network?.id ?? DEFAULT_NETWORK_ID
-			);
 
 			setContracts({
 				AelinPoolFactory: new Contract(
@@ -49,26 +30,6 @@ const useContractsInterface = () => {
 					poolFactoryContract.abi,
 					signer
 				),
-				AelinStaking: {
-					StakingContract: new Contract(
-						aelinStakingRewardsContract.address,
-						aelinStakingRewardsContract.abi,
-						signer
-					),
-					TokenContract: new Contract(aelinTokenContract.address, aelinTokenContract.abi, signer),
-				},
-				AelinEthStaking: {
-					StakingContract: new Contract(
-						aelinEthStakingRewardsContract.address,
-						aelinEthStakingRewardsContract.abi,
-						signer
-					),
-					TokenContract: new Contract(
-						aelinEthLPTokenContract.address,
-						aelinEthLPTokenContract.abi,
-						signer
-					),
-				},
 			});
 		} catch (e) {
 			console.log('Could not find Aelin contracts for this network', e);
