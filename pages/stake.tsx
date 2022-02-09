25820f4002076567d5b775947f4f3795eb1730ee
0xcb <>
2022-02-09 14:12:29 -0500

    uniswap URL updated

--- pages/stake.tsx
+++ pages/stake.tsx
@@ -117,7 +117,7 @@ const Stake = () => {
 							<SubmitButton
 								onClick={() =>
 									window.open(
-										'https://www.sorbet.finance/#/pools/0x665d8D87ac09Bdbc1222B8B9E72Ddcb82f76B54A',
+										'https://app.uniswap.org/#/add/0xa9c125bf4c8bb26f299c00969532b66732b1f758/ETH/10000?chain=mainnet',
 										'blank'
 									)
 								}
