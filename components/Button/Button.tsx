ed32100ab77466086f071ae0af24904203e32a9d
saeta.eth <sebastianlorenzo@gmail.com>
2022-02-10 15:42:17 -0300

    Add button refactor

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,21 +1,17 @@
-//@ts-nocheck
 import styled, { css } from 'styled-components';
 import { resetButtonCSS } from 'components/common';
-import Color from 'color';
 
 type ButtonProps = {
 	size?: 'sm' | 'md' | 'lg' | 'xl';
-	variant?: 'primary' | 'secondary' | 'tertiary' | 'solid' | 'outline' | 'text' | 'round';
+	variant?: 'primary' | 'secondary' | 'tertiary';
 	isActive?: boolean;
 	isRounded?: boolean;
 };
 
 const Button = styled.button<ButtonProps>`
-	font-family: ${(props) => props.theme.fonts.ASMRegular};
 	height: 32px;
 	line-height: 32px;
 	font-size: 1rem;
-	padding: 0 12px;
 	border: none;
 	border-radius: ${(props) => (props.isRounded ? '100px' : '4px')};
 	white-space: nowrap;
@@ -23,13 +19,6 @@ const Button = styled.button<ButtonProps>`
 	outline: none;
 	color: ${(props) => props.theme.colors.white};
 	text-transform: capitalize;
-
-	&:disabled {
-		background: ${(props) => Color(props.theme.colors.grey).alpha(0.5).rgb().string()};
-		color: ${(props) => props.theme.colors.white};
-		box-shadow: none;
-		cursor: not-allowed;
-	}
 
 	${(props) =>
 		props.size === 'sm' &&
@@ -48,7 +37,6 @@ const Button = styled.button<ButtonProps>`
 	${(props) =>
 		props.size === 'lg' &&
 		css`
-			padding: 0 40px;
 			height: 40px;
 			line-height: 40px;
 		`}
@@ -62,110 +50,50 @@ const Button = styled.button<ButtonProps>`
 		`}
 
 	${(props) =>
-		props.variant === 'round' &&
-		css`
-			color: ${(props) => props.theme.colors.white};
-			background-color: ${(props) => props.theme.colors.forestGreen};
-			border-radius: 30px;
-			&:hover {
-				&:not(:disabled) {
-					cursor: pointer;
-				}
-			}
-		`}
-
-	${(props) =>
 		props.variant === 'primary' &&
 		css`
-			color: ${(props) => props.theme.colors.black};
-			background: ${(props) => props.theme.colors.grey};
-			box-shadow: 0px 0px 10px rgba(0, 209, 255, 0.6);
-			border: 1px solid transparent;
+			color: ${(props) => props.theme.colors.white};
+			background: ${(props) => props.theme.colors.forestGreen};
+
 			&:hover {
 				&:not(:disabled) {
-					background: ${(props) => props.theme.colors.forestGreen};
-					box-shadow: 0px 0px 10px rgba(0, 209, 255, 0.9);
-					border: 1px solid ${(props) => props.theme.colors.buttonStroke};
+					box-shadow: 0px 0px 10px rgba(63, 121, 35, 0.5);
 				}
+			}
+
+			&:disabled {
+				opacity: 0.5;
+				box-shadow: none;
+				cursor: not-allowed;
 			}
 		`}
 
 		${(props) =>
 		props.variant === 'secondary' &&
 		css`
-			color: ${(props) => props.theme.colors.textGrey};
-			background: ${(props) => props.theme.colors.grey};
-			box-shadow: 0px 0px 10px rgba(0, 209, 255, 0.9);
-			border: 1px solid ${(props) => props.theme.colors.buttonStroke};
+			background: transparent;
+			border: 1px solid ${(props) => props.theme.colors.forestGreen};
+			color: ${(props) => props.theme.colors.forestGreen};
+
 			&:hover {
 				&:not(:disabled) {
-					background: ${(props) => props.theme.colors.forestGreen};
-					color: ${(props) => props.theme.colors.black};
+					box-shadow: 0px 0px 10px rgba(63, 121, 35, 0.5);
 				}
+			}
+
+			&:disabled {
+				opacity: 0.5;
+				box-shadow: none;
+				cursor: not-allowed;
 			}
 		`}
 
 		${(props) =>
 		props.variant === 'tertiary' &&
 		css`
-			color: ${(props) => props.theme.colors.textGrey};
-			background: ${(props) => props.theme.colors.grey};
-			box-shadow: 0px 0px 15px rgba(237, 30, 255, 0.6);
-			border: 1px solid ${(props) => props.theme.colors.buttonStroke};
-			&:hover {
-				&:not(:disabled) {
-					background: ${(props) => props.theme.colors.textGrey};
-					color: ${(props) => props.theme.colors.black};
-				}
-			}
-		`}
-
-
-		${(props) =>
-		props.variant === 'solid' &&
-		css`
-			color: ${(props) => props.theme.colors.white};
-			background: ${(props) => props.theme.colors.grey};
-			&:hover {
-				&:not(:disabled) {
-					background: ${(props) => props.theme.colors.forestGreen};
-				}
-			}
-			&:disabled {
-				background: ${(props) => Color(props.theme.colors.grey).alpha(0.2).rgb().string()};
-			}
-		`}
-
-
-		${(props) =>
-		props.variant === 'outline' &&
-		css`
-			background: transparent;
-			color: ${(props) => props.theme.colors.forestGreen};
-			border: 1px solid ${(props) => props.theme.colors.forestGreen};
-
-			&:hover {
-				&:not(:disabled) {
-					color: ${(props) => props.theme.colors.white};
-					background: ${(props) => props.theme.colors.forestGreen};
-				}
-			}
-
-			&:disabled {
-				border: none;
-				opacity: 0.5;
-				color: ${(props) => props.theme.colors.headerGrey};
-				border: 1px solid ${(props) => props.theme.colors.headerGrey};
-				background: ${(props) => Color(props.theme.colors.grey).alpha(0.2).rgb().string()};
-			}
-		`}
-
-
-		${(props) =>
-		props.variant === 'text' &&
-		css`
 			${resetButtonCSS};
 			color: ${(props) => props.theme.colors.forestGreen};
+
 			&:hover {
 				&:not(:disabled) {
 					color: ${(props) => props.theme.colors.forestGreen};
