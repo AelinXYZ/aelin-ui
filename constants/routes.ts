84a20b3a0c5ab7eb907587fb5ba11e2bc3c85cef
saeta.eth <sebastianlorenzo@gmail.com>
2022-02-09 21:26:38 -0300

    Add uniswap links

--- constants/routes.ts
+++ constants/routes.ts
@@ -3,8 +3,10 @@ const ROUTES = {
 	Stake: '/stake',
 	ClaimTokens: '/claim-tokens',
 	Docs: 'https://docs.aelin.xyz',
-	UniswapPool:
+	UniswapPoolOP:
 		'https://info.uniswap.org/#/optimism/pools/0x5e8b0fc35065a5d980c11f96cb52381de390b13f',
+	UniswapPoolL1:
+		'https://app.uniswap.org/#/swap?outputCurrency=0xa9c125bf4c8bb26f299c00969532b66732b1f758&inputCurrency=ETH&chain=mainnet',
 	Pools: {
 		Home: '/pools',
 		Create: '/pools/create',
