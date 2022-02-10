f46e1bd501b4ec94d344ae6374a56fbe1c87749b
0xcb <>
2022-02-09 19:05:03 -0500

    uniswap APY

--- queries/stakingRewards/useGetUniswapStakingRewardsAPY.ts
+++ queries/stakingRewards/useGetUniswapStakingRewardsAPY.ts
@@ -1,12 +1,12 @@
 import { ethers } from 'ethers';
 import { request, gql } from 'graphql-request';
 import { useQuery } from 'react-query';
+import erc20Abi from 'contracts/erc20';
 
 import Connector from 'containers/Connector';
 
+const ONE_YEAR = 365 * 24 * 3600;
 const GRAPH_ENDPOINT = 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2';
-const TOKEN_0 = '0xa9c125bf4c8bb26f299c00969532b66732b1f758';
-const TOKEN_1 = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
 const PAIR_ID = '0x974d51fafc9013e42cbbb9465ea03fe097824bcc';
 
 const useGetUniswapStakingRewardsAPY = ({
@@ -26,6 +26,8 @@ const useGetUniswapStakingRewardsAPY = ({
 		],
 		async () => {
 			try {
+				const UniV2Contract = new ethers.Contract(tokenContract?.address!, erc20Abi, provider);
+
 				const query = gql`
 					query getPair($id: String!) {
 						pair(id: $id) {
@@ -41,13 +43,42 @@ const useGetUniswapStakingRewardsAPY = ({
 					id: PAIR_ID,
 				};
 
-				const data = await request(GRAPH_ENDPOINT, query, variables);
+				const [
+					uniV2TotalSupply,
+					ratesResults,
+					graphData,
+					rewardForDuration,
+					duration,
+					contractBalance,
+				] = await Promise.all([
+					UniV2Contract.totalSupply(),
+					fetch(
+						'https://api.coingecko.com/api/v3/simple/price?ids=aelin%2Cethereum&vs_currencies=usd'
+					),
+					request(GRAPH_ENDPOINT, query, variables),
+					stakingRewardsContract?.getRewardForDuration(),
+					stakingRewardsContract?.rewardsDuration(),
+					tokenContract?.balanceOf(stakingRewardsContract?.address),
+				]);
+				const {
+					pair: { reserve0: amount0, reserve1: amount1 },
+				} = graphData;
+				const {
+					aelin: { usd: aelinRate },
+					ethereum: { usd: ethRate },
+				} = await ratesResults.json();
 
-				const totalValueInPool =
-					(amount0Current / 1e18) * ethRate + (amount1Current / 1e18) * aelinRate;
+				const totalValueInPool = Number(amount0) * aelinRate + Number(amount1) * ethRate;
+				const uniV2Price = totalValueInPool / (uniV2TotalSupply / 1e18);
+				const yearProRata = ONE_YEAR / Number(duration);
+				const uniV2ValueInContract = (contractBalance / 1e18) * uniV2Price;
+				const rewardsValuePerYear = (rewardForDuration / 1e18) * yearProRata * aelinRate;
 
-				console.log(data);
-				return data;
+				return {
+					aelin: Number(amount0),
+					eth: Number(amount1),
+					apy: (100 * rewardsValuePerYear) / uniV2ValueInContract,
+				};
 			} catch (e) {
 				console.log(e);
 			}
