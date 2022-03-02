376713f3bcf29682b5f699d990e94e030f2e8426
saeta.eth <SebastianLorenzo@gmail.com>
2022-03-01 22:28:28 -0300

    Fix selected tab (#317)
    * Fix selected tab
    
    * remove console log
    
    Co-authored-by: saeta.eth <saeta@aelindao.xyz>

--- sections/Pools/ViewPool.tsx
+++ sections/Pools/ViewPool.tsx
@@ -229,8 +229,10 @@ const ViewPool: FC<ViewPoolProps> = ({ pool, poolAddress }) => {
 		return (
 			(pool?.poolStatus === Status.FundingDeal && deal.holderFundingExpiration <= now) ||
 			(pool?.poolStatus !== Status.FundingDeal &&
+				pool?.purchaseExpiry != null &&
+				deal?.id != null &&
 				now > (pool?.purchaseExpiry ?? 0) + (pool?.duration ?? 0) &&
-				!(pool?.poolStatus === Status.DealOpen && deal?.id != null))
+				!(pool?.poolStatus === Status.DealOpen))
 		);
 	}, [
 		deal.holderFundingExpiration,
