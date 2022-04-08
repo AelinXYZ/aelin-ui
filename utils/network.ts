d17034e1d618eca8ce297cebdb5d8c6b8af2d3cc
alextheboredape <89657259+alextheboredape@users.noreply.github.com>
2022-04-07 17:40:49 -0700

    Stake gas (#385)
    * add staking multiplier
    
    * Add extra gas to stake txs
    
    Co-authored-by: saeta.eth <saeta@aelindao.xyz>

--- utils/network.ts
+++ utils/network.ts
@@ -2,6 +2,7 @@ import Wei, { wei } from '@synthetixio/wei';
 import { GasLimitEstimate, GWEI_UNIT } from 'constants/networks';
 
 const GAS_LIMIT_BUFFER_MULTIPLIER = 20;
+const STAKING_EXTRA_MULTIPLIER = 15;
 
 export const getTransactionPrice = (
 	gasPrice: Wei | null,
@@ -14,9 +15,17 @@ export const getTransactionPrice = (
 	return gasPrice.mul(ethPrice).mul(gasLimit).div(GWEI_UNIT).toNumber().toFixed(4);
 };
 
-export const getGasEstimateWithBuffer = (gasEstimate: GasLimitEstimate) => {
+export const getGasEstimateWithBuffer = (
+	gasEstimate: GasLimitEstimate,
+	isStaking: boolean = false
+) => {
 	if (!gasEstimate) {
 		return null;
 	}
-	return gasEstimate?.add(gasEstimate?.mul(wei(GAS_LIMIT_BUFFER_MULTIPLIER, 0)).div(100)) ?? null;
+	const newEstimate =
+		gasEstimate?.add(gasEstimate?.mul(wei(GAS_LIMIT_BUFFER_MULTIPLIER, 0)).div(100)) ?? null;
+
+	return isStaking
+		? newEstimate?.add(newEstimate?.mul(wei(STAKING_EXTRA_MULTIPLIER, 0)).div(100))
+		: newEstimate;
 };
